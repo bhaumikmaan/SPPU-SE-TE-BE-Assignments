df29b2d5b3f1f6aac62be4ef48e7ea53fd517251
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-11-29 19:31:46 +0530

    Create Disk Scheduling.c

---
+++ Semester 5/OS/Assignment 8/Disk Scheduling.c
@@ -0,0 +1,215 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int RQ[100],i,n,TotalHeadMoment=0,initial,count=0;
+
+void getData()
+{
+    printf("Enter the number of Requests\n");
+    scanf("%d",&n);
+    printf("Enter the Requests sequence\n");
+    for(i=0;i<n;i++)
+     scanf("%d",&RQ[i]);
+    printf("Enter initial head position\n");
+    scanf("%d",&initial);
+}
+void sstf()
+{
+    while(count!=n)
+    {
+        int min=1000,d,index;
+        for(i=0;i<n;i++)
+        {
+           d=abs(RQ[i]-initial);
+           if(min>d)
+           {
+               min=d;
+               index=i;
+           }
+           
+        }
+        TotalHeadMoment=TotalHeadMoment+min;
+        initial=RQ[index];
+        // 1000 is for max
+        // you can use any number
+        RQ[index]=1000;
+        count++;
+    }
+    
+    printf("Total head movement is %d",TotalHeadMoment);
+}
+
+void scan()
+{
+
+printf("Enter total disk size\n");
+    scanf("%d",&size);
+    printf("Enter the head movement direction for high 1 and for low 0\n");
+    scanf("%d",&move);
+    
+    // logic for Scan disk scheduling
+    
+        /*logic for sort the request array */
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(RQ[j]>RQ[j+1])
+            {
+                int temp;
+                temp=RQ[j];
+                RQ[j]=RQ[j+1];
+                RQ[j+1]=temp;
+            }
+
+        }
+    }
+
+    int index;
+    for(i=0;i<n;i++)
+    {
+        if(initial<RQ[i])
+        {
+            index=i;
+            break;
+        }
+    }
+   
+    // if movement is towards high value
+    if(move==1)
+    {
+        for(i=index;i<n;i++)
+        {
+            TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+            initial=RQ[i];
+        }
+        //  last movement for max size 
+        TotalHeadMoment=TotalHeadMoment+abs(size-RQ[i-1]-1);
+        initial = size-1;
+        for(i=index-1;i>=0;i--)
+        {
+             TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+             initial=RQ[i];
+            
+        }
+    }
+    // if movement is towards low value
+    else
+    {
+        for(i=index-1;i>=0;i--)
+        {
+            TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+            initial=RQ[i];
+        }
+        //  last movement for min size 
+        TotalHeadMoment=TotalHeadMoment+abs(RQ[i+1]-0);
+        initial =0;
+        for(i=index;i<n;i++)
+        {
+             TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+             initial=RQ[i];
+            
+        }
+    }
+    
+    printf("Total head movement is %d",TotalHeadMoment);
+}
+
+void clook()
+{
+printf("Enter total disk size\n");
+    scanf("%d",&size);
+    printf("Enter the head movement direction for high 1 and for low 0\n");
+    scanf("%d",&move);
+    
+    // logic for C-look disk scheduling
+    
+        /*logic for sort the request array */
+    for(i=0;i<n;i++)
+    {
+        for( j=0;j<n-i-1;j++)
+        {
+            if(RQ[j]>RQ[j+1])
+            {
+                int temp;
+                temp=RQ[j];
+                RQ[j]=RQ[j+1];
+                RQ[j+1]=temp;
+            }
+
+        }
+    }
+
+    int index;
+    for(i=0;i<n;i++)
+    {
+        if(initial<RQ[i])
+        {
+            index=i;
+            break;
+        }
+    }
+   
+    // if movement is towards high value
+    if(move==1)
+    {
+        for(i=index;i<n;i++)
+        {
+            TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+            initial=RQ[i];
+        }
+        
+        for( i=0;i<index;i++)
+        {
+             TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+             initial=RQ[i];
+            
+        }
+    }
+    // if movement is towards low value
+    else
+    {
+        for(i=index-1;i>=0;i--)
+        {
+            TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+            initial=RQ[i];
+        }
+        
+        for(i=n-1;i>=index;i--)
+        {
+             TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+             initial=RQ[i];
+            
+        }
+    }
+    
+    printf("Total head movement is %d",TotalHeadMoment);
+    return 0;
+}
+int main()
+{
+    int choice;
+    while(1)
+    {
+        printf("\nDisk Scheduling Alogrithms\n1.Enter data\n2.SSTF\n3.SCAN\n4.C-LOOK\n\n5.Exit\nEnter your choice:");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+        case 1:
+            getData();
+            break;
+        case 2:
+            sstf();
+            break;
+        case 3:
+            scan();
+            break;
+        case 4:
+            clook();
+            break;
+        default:
+            return 0;
+            break;
+        }
+    }
+}
